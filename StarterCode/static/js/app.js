be465f669b318ab6f149b5d2c580eadc00a095ce
Ugochi <uakaluso@gmail.com>
2019-05-19 18:20:47 -0500

    updating app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,31 +1,37 @@
 // from data.js
 var tableData = data;
-//console.log(tableData);
+// console.log(tableData);
 
 // YOUR CODE HERE!
 
 // Loop through array of objects then each object
 var tbody = d3.select("tbody");
 
-tableData.forEach((datum) => {
-    var row = tbody.append("tr");
-    Object.entries(datum).forEach(([_, value]) => {
-      var cell = row.append("td").text(value);
-    });
-  });
-  
+function displayData(data){ 
+  tbody.text("")
+  data.forEach((datum) => {
+      var row = tbody.append("tr");
+      Object.entries(datum).forEach(([_, value]) => {
+        var cell = row.append("td").text(value);
+      });
+    })};
+
+displayData(tableData)
+
   // Use a date form in your HTML document and write JavaScript code that will listen for events 
   // and search through the date/time column to find rows that match user input.
   
-  var input = d3.select("#datetime");
-
-  input.on("input", function() {
-    var inputValue = d3.event.target.value;
-    console.log(inputValue);
+  var btn= d3.select("#filter-btn");
+  var input = d3.select("#datetime")
   
+    
+  // filter data with desired date
+    function filter(){
+        d3.event.preventDefault();
+        var inputValue= input.property("value")
+        console.log(inputValue);
+        var filteredData = tableData.filter(datum => datum.datetime===inputValue)
+        displayData(filteredData)
+    }
 
-  // var inputValue = input.property("value");
-
-  var filteredData = tableData.filter(tableData => tableData.datetime=== inputValue)
-  console.log(filteredData)
-});
+    btn.on("click", filter)
